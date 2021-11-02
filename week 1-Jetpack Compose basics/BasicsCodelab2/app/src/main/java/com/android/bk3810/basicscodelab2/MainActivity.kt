d923ec8c058c40cec13ee240bd4e7a40ec4053f6
BK <bkkim@linkflow.co.kr>
2021-11-02 20:20:22 +0900

    week1 - 튜토리얼 강의.4

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/android/bk3810/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/android/bk3810/basicscodelab2/MainActivity.kt
@@ -4,17 +4,23 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +33,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicsCodelab2Theme {
                 //Text("Hello World!!!")
-                MessageCard(Message("Android", "Jetpack Compose"))
+                //MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(messages = SampleData.conversationSample)
             }
 
 //            BasicsCodelab2Theme {
@@ -71,9 +78,15 @@ fun MessageCard(msg: Message) {
         )
 
         // 수평 공백 추가
-        Spacer(modifier = Modifier.width(10.dp))
+        Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false)}
+
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -82,11 +95,17 @@ fun MessageCard(msg: Message) {
             // 수직 공백 추가
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                 )
             }
         }
@@ -105,5 +124,22 @@ fun PreviewMessageCard() {
         MessageCard(
             msg = Message("Colleague", "Hey, take a look at Jetpack Compose")
         )
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { messages ->
+            MessageCard(msg = messages)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    BasicsCodelab2Theme {
+        Conversation(SampleData.conversationSample)
     }
 }
---
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/android/bk3810/basicscodelab2/SampleData.kt
@@ -0,0 +1,13 @@
+package com.android.bk3810.basicscodelab2
+
+object SampleData {
+    val conversationSample = listOf(
+        Message("Colleague",
+        "Test...Test...Test"
+        ),
+        Message("Colleague",
+            "Test2...Test2...Test2"
+        ),
+
+    )
+}
