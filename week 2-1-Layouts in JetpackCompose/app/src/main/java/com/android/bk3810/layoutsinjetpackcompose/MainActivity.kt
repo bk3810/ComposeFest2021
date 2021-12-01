af7cda5e74617d2a82535e2419a7b2055567d945
BK <bkkim@linkflow.co.kr>
2021-12-01 10:14:24 +0900

    week-2 5

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/android/bk3810/layoutsinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/android/bk3810/layoutsinjetpackcompose/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -39,6 +41,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun LayoutsCodelab() {
+    Scaffold (
+        topBar = {
+            TopAppBar (
+                title = {
+                    Text(text = "Layouts Codelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+
+            )
+        }
+    ){ innerPadding ->
+        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutsInJetpackComposeTheme {
+        LayoutsCodelab()
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
@@ -58,7 +98,7 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
             .padding(8.dp)
             .clip(RoundedCornerShape(4.dp))
             .background(MaterialTheme.colors.surface)
-            . clickable ( onClick = { /* Ignoring onclick */ } )
+            .clickable(onClick = { /* Ignoring onclick */ })
             .padding(16.dp)
             ){
         Surface(
